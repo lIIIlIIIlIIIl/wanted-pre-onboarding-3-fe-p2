75144e0b3f4a8eafbb953b90789f6f337c6e2646
ShinDongJae <sdhsdj6450@gmail.com>
2023-03-10 13:56:45 +0900

    문제 해결

--- src/api/login.ts
+++ src/api/login.ts
@@ -1,48 +1,73 @@
-import { BASE_URL } from './const'
-import { getAccessTokenFromLocalStorage, saveAccessTokenToLocalStorage } from '../utils/accessTokenHandler'
-import { UserInfo } from '../types/user'
+import { BASE_URL } from "./const";
+import {
+  getAccessTokenFromLocalStorage,
+  saveAccessTokenToLocalStorage,
+} from "../utils/accessTokenHandler";
+import { UserInfo } from "../types/user";
 
-type LoginResult = 'success' | 'fail'
+type LoginResult = "success" | "fail";
 
-export type LoginResultWithToken = {
-  result: 'success'
-  access_token: string
-} | {
-  result: 'fail'
-  access_token: null
-}
+export type LoginResultWithToken =
+  | {
+      result: "success";
+      access_token: string;
+    }
+  | {
+      result: "fail";
+      access_token: null;
+    };
 
 export interface LoginRequest {
-  username: string
-  password: string
+  username: string;
+  password: string;
 }
 
 /*********
  *  실습 2-1
  * */
 
-export const loginWithToken = async (args: LoginRequest): Promise<LoginResultWithToken> => {
+export const loginWithToken = async (
+  args: LoginRequest
+): Promise<LoginResultWithToken> => {
   // TODO(2-1): 로그인 API 호출 및 토큰 반환하기
-  // POST, `${ BASE_URL }/auth/login`을 호출하세요.
-  // API Spec은 강의 자료를 참고하세요.
-  // access_token 발급에 성공한 경우에는 { result: 'success', access_token: string } 형태의 값을 반환하세요.
 
-  return {
-    result: 'fail',
-    access_token: null
+  const loginRes = await fetch(`${BASE_URL}/auth/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(args),
+  });
+
+  if (loginRes.ok) {
+    const loginResponseData = await loginRes.json();
+    return { result: "success", access_token: loginResponseData.access_token };
   }
-}
+  return {
+    result: "fail",
+    access_token: null,
+  };
+};
 
-export const getCurrentUserInfoWithToken = async (token: string): Promise<UserInfo | null> => {
+export const getCurrentUserInfoWithToken = async (
+  token: string
+): Promise<UserInfo | null> => {
   // TODO(2-1): 함수에서 토큰을 직접 주입받아 사용하기
-  // GET, `${ BASE_URL }/profile`을 호출하세요.
-  // argument로 전달받은 token을 Authorization header에 Bearer token으로 넣어주세요.
-  // API Spec은 강의 자료를 참고하세요.
-  // 유저 정보 조회에 성공한 경우에는 UserInfo 타입의 값을 반환하세요.
 
-  return null
-}
+  let getCurrentUserRes = await fetch(`${BASE_URL}/profile`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
 
+  if (getCurrentUserRes.ok) {
+    return getCurrentUserRes.json() as Promise<UserInfo>;
+  }
+
+  return null;
+};
 
 /*********
  *  실습 2-2
@@ -50,19 +75,38 @@ export const getCurrentUserInfoWithToken = async (token: string): Promise<UserIn
 
 export const login = async (args: LoginRequest): Promise<LoginResult> => {
   // TODO(2-2): 로그인 API 호출 및 access token 로컬스토리지에 저장하기
-  // POST, `${ BASE_URL }/auth/login`을 호출하세요.
-  // API Spec은 강의 자료를 참고하세요.
-  // access_token 발급에 성공한 경우에는 saveAccessTokenToLocalStorage 함수를 호출하여 access_token을 localStorage에 저장하고 'success'를 반환하세요.
+  const loginRes = await fetch(`${BASE_URL}/auth/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(args),
+  });
 
-  return 'fail'
-}
+  if (loginRes.ok) {
+    const loginResponseData = await loginRes.json();
+    saveAccessTokenToLocalStorage(loginResponseData.access_token);
+    return "success";
+  }
+
+  return "fail";
+};
 
 export const getCurrentUserInfo = async (): Promise<UserInfo | null> => {
   // TODO(2-2): 로컬스토리지에서 토큰을 가져와 사용하기
-  // GET, `${ BASE_URL }/profile`을 호출하세요.
-  // 로컬 스토리지에 있는 token을 getAccessTokenFromLocalStorage로 가져와서 Authorization header에 Bearer token으로 넣어주세요.
-  // API Spec은 강의 자료를 참고하세요.
-  // 유저 정보 조회에 성공한 경우에는 UserInfo 타입의 값을 반환하세요.
+  let token = getAccessTokenFromLocalStorage();
 
-  return null
-}
+  let getCurrentUserRes = await fetch(`${BASE_URL}/profile`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  if (getCurrentUserRes.ok) {
+    return getCurrentUserRes.json() as Promise<UserInfo>;
+  }
+
+  return null;
+};
